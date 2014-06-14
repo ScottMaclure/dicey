fff71b577b851342ccbedcb8d8bdc0be40cf5516
Scott Maclure <scott@maclure.com.au>
2014-06-14 16:47:51 +1000

    #1 Update render to support diceGroups.

--- javascripts/jsx/DiceBar.js
+++ javascripts/jsx/DiceBar.js
@@ -26,29 +26,43 @@ exports.DiceBar = React.createClass({
 
 		// FIXME Why isn't there a simpler way to pass dynamic properties for clicks?
 		var die = $(this.getDOMNode()).find('[data-reactid="' + arguments[1] + '"]').text();
-		
+
 		this.props.onDieRoll(die);
 	},
 
 	render: function () {
 
-		var diceButtons = this.props.dice.map(function (die) {
+		var self = this;
+
+		var diceGroups = self.props.diceGroups.map(function (diceGroup) {
+
+			var currentGroup = diceGroup.map(function (die) {
+				/*jshint ignore:start*/
+				return (
+					<button
+						type="button"
+						className="btn btn-primary"
+						onClick={self.handleClick}
+					>{die}</button>
+				);
+				/*jshint ignore:end*/
+			});
+
 			/*jshint ignore:start*/
 			return (
-				<button 
-					type="button" 
-					className="btn btn-primary"
-					onClick={this.handleClick}
-				>{die}</button>
+				<div className="btn-group-vertical btn-group-lg">
+					{currentGroup}
+				</div>
 			);
 			/*jshint ignore:end*/
-		}.bind(this));
+
+		});
 
 		/*jshint ignore:start*/
 		return (
 			<div className="diceBar">
-				<div className="btn-group-vertical btn-group-lg">
-					{diceButtons}
+				<div className="btn-toolbar" role="toolbar">
+					{diceGroups}
 				</div>
 			</div>
 		);
