eb67ab3a310d418649468429faf922c062370173
Scott Maclure <scott@maclure.com.au>
2014-06-14 16:47:38 +1000

    #1 Refactored model to use diceGroups.

--- javascripts/dicey.js
+++ javascripts/dicey.js
@@ -8,8 +8,21 @@ var diceyApp = require('./build/DiceyApp.js').DiceyApp;
 // TODO Load from JSON file? From LocalStorage?
 var data = {
 	pageTitle: 'Dicey, a die roller.',
-	dice: [
-		'1d4', '1d6', '2d6', '1d8', '1d10', '1d12', '1d20', '1d100'
+	diceGroups: [
+		[
+			'1d4',
+			'1d6',
+			'1d8',
+			'1d10',
+			'1d12',
+			'1d20',
+			'1d100'
+		],
+		[
+			'2d6',
+			'2d10',
+			'17d10'
+		]
 	],
 	resultsLog: []
 };
