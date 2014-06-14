e09598f53ef6c4109bbbe4e95e48d9e053f18a9c
Scott Maclure <scott@maclure.com.au>
2014-06-14 10:22:11 +1000

    #9 Created ResultModal component. Not using it yet.

---
+++ javascripts/jsx/ResultModal.js
@@ -0,0 +1,26 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+
+exports.ResultModal = React.createClass({
+
+	render: function () {
+
+		/*jshint ignore:start*/
+		return (
+			<div id="ResultModal" className="modal fade" 
+				tabindex="-1" role="dialog" aria-labelledby="ResultModal" aria-hidden="true">
+				<div className="modal-dialog modal-sm">
+					<div className="modal-content">
+						TODO results go in here.
+					</div>
+				</div>
+			</div>
+		);
+		/*jshint ignore:end*/
+
+	}
+
+});
