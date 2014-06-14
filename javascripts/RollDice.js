e3e22d650d51c5039e9b5b4b67834f4ff0e92467
Scott Maclure <scott@maclure.com.au>
2014-06-14 11:16:18 +1000

    #9 Added zeroPad helper method, for formatting timestamps.

--- javascripts/RollDice.js
+++ javascripts/RollDice.js
@@ -1,6 +1,6 @@
 /**
  * RollDice - JavaScript die roller, with an API based off the "rolldice" program in Linux:
- * 
+ *
  * http://linux.die.net/man/1/roll
  * https://github.com/sstrickl/rolldice
  *
@@ -10,13 +10,29 @@
 exports.RollDice = {
 
 	roll: function (die) {
-		
+
 		var parts = die.trim().split('d');
 		var dieType = parseInt(parts[1], 10); // The sides: 4, 6, 8, 10, etc.
 
 		var result = Math.floor(Math.random() * dieType) + 1;
 
-		return result;		
+		return result;
+	},
+
+	/**
+	 * TODO Move somewhere else.
+	 */
+	zeroPad: function (n, w) {
+
+		var n_ = Math.abs(n);
+		var zeros = Math.max(0, w - Math.floor(n_).toString().length);
+		var zeroString = Math.pow(10, zeros).toString().substr(1);
+
+		if (n < 0) {
+			zeroString = '-' + zeroString;
+		}
+
+		return zeroString + n;
 	}
 
 };
