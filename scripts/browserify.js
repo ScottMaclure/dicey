8885f470d9330797d2146e2549cf1cf399c25df5
Scott Maclure <scott@maclure.com.au>
2016-04-21 12:59:21 +1000

    Add CLI script to watch/bundle the app.

---
+++ scripts/browserify.js
@@ -0,0 +1,26 @@
+var fs = require('fs'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	babelify = require('babelify');
+
+var isDebugMode = process.env.NODE_ENV === 'development';
+
+var b = browserify({
+		entries: ['./javascripts/dicey.js'],
+		// sourcemaps
+		debug: isDebugMode,
+		verbose: true,
+		cache: {},
+		packageCache: {},
+		plugin: [watchify]
+	}).on('update', bundle);
+
+function bundle() {
+	console.log('browserify+babelify running...');
+	b.transform('babelify', {presets: ['react']})
+	.bundle()
+	.pipe(fs.createWriteStream('public/bundle.js'));
+}
+
+// Kick things off.
+bundle();
