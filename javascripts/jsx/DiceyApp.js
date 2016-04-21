80f6d323fa7c8015e82c738b900b2ded3f971e4a
Scott Maclure <scott@maclure.com.au>
2016-04-21 20:38:05 +1000

    layout adjust, update for data model.

--- javascripts/jsx/DiceyApp.js
+++ javascripts/jsx/DiceyApp.js
@@ -29,7 +29,7 @@ exports.DiceyApp = React.createClass({
 		pageTitle: React.PropTypes.string,
 		resultsLog: React.PropTypes.arrayOf(React.PropTypes.string),
 		latestResult: React.PropTypes.object,
-		diceGroups: React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.string))
+		diceGroups: React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.object))
 	},
 
 	getInitialState: function () {
@@ -52,34 +52,26 @@ exports.DiceyApp = React.createClass({
 	render: function () {
 		/*jshint ignore:start*/
 		return (
-			<div className="container">
 
-				<div className="diceyApp">
+			<div className="diceyApp">
 
-					<PageHeader title={this.props.pageTitle}/>
+				<PageHeader title={this.props.pageTitle}/>
 
-					<div className="row">
-						<div className="col-xs-3 col-md-1">
-							&nbsp;
+				<div className="container">
+
+					<div className="row mainRow">
+						<div className="col-xs-2 col-sm-2 text-left mainRowLeft">
+							<DiceBar diceGroups={this.props.diceGroups.slice(0, 1)} onDieRoll={this.handleDieRoll}/>
 						</div>
-						<div className="col-xs-9 col-md-11">
+						<div className="col-xs-8 col-sm-8 mainRowCenter">
 							<ResultsActions
 								onResultsClear={this.handleResultsClear}
 								onResultsSpace={this.handleResultsSpace}
 							/>
-						</div>
-					</div>
-
-					<div className="row">
-						<div className="col-xs-3 col-md-1">
-
-							<DiceBar diceGroups={this.props.diceGroups} onDieRoll={this.handleDieRoll}/>
-
-						</div>
-						<div className="col-xs-9  col-md-11">
-
 							<ResultsLog log={this.state.resultsLog} />
-
+						</div>
+						<div className="col-xs-2 col-sm-2 text-right mainRowRight">
+							<DiceBar diceGroups={this.props.diceGroups.slice(1, 2)} onDieRoll={this.handleDieRoll}/>
 						</div>
 					</div>
 
@@ -90,6 +82,10 @@ exports.DiceyApp = React.createClass({
 					dieResult={this.state.latestResult.dieResult}
 					dieString={this.state.latestResult.dieString}
 				/>
+
+				<footer className="text-center">
+					<a href="https://github.com/ScottMaclure/dicey">Fork me on GitHub</a>
+				</footer>
 
 			</div>
 		);
