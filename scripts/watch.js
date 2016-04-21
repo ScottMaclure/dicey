d1207541b6fa50e905f48050736661c32186569a
Scott Maclure <scott@maclure.com.au>
2016-04-21 17:00:39 +1000

    Setting up sw-precache.

---
+++ scripts/watch.js
@@ -0,0 +1,87 @@
+/**
+ * npm run watch - script to regen app and service-worker.
+ * TODO browserify/watchify only listens to app js. More?
+ */
+
+var fs = require('fs'),
+	path = require('path'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	babelify = require('babelify'),
+	swPrecache = require('sw-precache');
+
+// Disable sourcemaps for now, etc.
+// var isDebugMode = process.env.NODE_ENV === 'development';
+var isDebugMode = false;
+
+var b = browserify({
+		entries: [path.join('javascripts', 'dicey.js')],
+		// sourcemaps
+		debug: isDebugMode,
+		cache: {},
+		packageCache: {},
+		plugin: [watchify]
+	})
+	.on('update', bundle);
+
+function formatTimestamp(d) {
+	return '[' + d.toLocaleString() + ']';
+}
+
+function log(msg) {
+	console.log(formatTimestamp(new Date()), msg);
+}
+
+function bundle() {
+
+	var start = Date.now();
+	log('watch.js: bundle task running...');
+
+	generateBundle().on('finish', () => {
+
+		generateServiceWorker().then(() => {
+			log('watch.js: complete in ' + ((Date.now() - start) / 1000) + 's');
+			log('---');
+		}).catch(function (err) {
+			log('ERROR generating service-worker!');
+		});
+
+	});
+
+}
+
+/**
+ * Rebuild ReactJS app and package for client browser.
+ * @return {stream.Writable} https://nodejs.org/api/stream.html#stream_class_stream_writable
+ */
+function generateBundle() {
+	log('generateBundle...');
+	return b
+	.transform('babelify', {presets: ['react']})
+	.bundle()
+	.on('error', (err) => {
+		log(err.message);
+		this.emit('end');
+	})
+	.pipe(fs.createWriteStream(path.join('public', 'bundle.js')))
+	.on('finish', () => {
+		log('generateBundle done.');
+	});
+}
+
+
+/**
+ * @return {Promise} Promise when finished.
+ */
+function generateServiceWorker() {
+	log('generateServiceWorker...');
+	return swPrecache.write(path.join('public', 'service-worker.js'), {
+		staticFileGlobs: ['public' + '/**/*.{js,html,css,png,jpg,gif}'],
+		stripPrefix: 'public'
+	}).then(function () {
+		log('generateServiceWorker done.');
+	});
+}
+
+// Kick things off to start.
+bundle();
