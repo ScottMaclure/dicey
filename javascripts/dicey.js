4f771df7ad8f945a7885a27ba90094e6474e0ef2
Scott Maclure <scott@maclure.com.au>
2016-04-21 21:05:58 +1000

    Tweaking dice order.

--- javascripts/dicey.js
+++ javascripts/dicey.js
@@ -14,23 +14,23 @@ var data = {
 			{die: '1d3'},
 			{die: '1d5'},
 			{die: '1d7'},
-			{die: '1d10'},
 			{die: '1d14'},
-			{die: '1d20', cssClass: 'btn-success'},
+			{die: '1d16'},
+			{die: '1d24'},
+			{die: '1d30'},
 			{die: '1d100', cssClass: 'btn-success'},
-			{die: '2d6', cssClass: 'btn-success'},
-			{die: '2d10'}
+			{die: '2d6', cssClass: 'btn-success'}
 		],
 		[
 			{die: '1d4'},
 			{die: '1d6'},
 			{die: '1d8'},
+			{die: '1d10'},
 			{die: '1d12'},
-			{die: '1d16'},
-			{die: '1d24'},
-			{die: '1d30'},
+			{die: '1d20', cssClass: 'btn-success'},
 			{die: '3d6', cssClass: 'btn-success'},
 			{die: '4d6'},
+			{die: '2d10'},
 			{die: '11d10'}
 		]
 	],
