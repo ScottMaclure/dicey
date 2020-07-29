767e1f714cc62fd50469b85dafafa2f0cda63790
Scott Maclure <scott.maclure@sourcedgroup.com>
2020-07-29 13:47:13 +0800

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,24 @@
 # Set prod mode
 export NODE_ENV="production"
 
+# Update deps
+npm prune
+npm install
+
+# Update version - always patch. Hrm...
+npm version patch -git-tag-version false
+
+# TODO Inject npm version into the frontend
+
 # Compile JS, in prod mode this won't watch, it will just build
 npm run watch
 
 # Upload to firebase hosting, requires npx firebase login
 npx firebase deploy
+
+# Update github, commit version update
+git commit -am "Deploy"
+git push
 
 # Remove production mode.
 unset NODE_ENV
